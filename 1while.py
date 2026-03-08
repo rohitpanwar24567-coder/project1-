81bf9250e1c709ec8e9e1d0e19c57ab044ed1d6e
Rohit Panwar <rohitpanwar24567@gmail.com>
2026-03-08 21:13:48 +0530

    Add files via upload

---
+++ 1while.py
@@ -0,0 +1,20 @@
+
+
+
+counter = 50
+  
+while counter > 0:
+
+  print(counter)
+  counter = counter - 1
+
+count = -1
+while count > -50:
+  print(count)
+  count -= 1
+  
+counter = 1
+while counter <=10:
+
+ print(counter *5 )
+ counter = counter + 1
